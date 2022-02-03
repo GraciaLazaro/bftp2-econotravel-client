34b199375a40cef58ac26865fc4b02b6ee85138f
Candy <candymdeoleo@gmail.com>
2022-02-03 16:06:11 +0100

    ECON 1 | visualizar tarjetas

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,32 +1,37 @@
 import React from "react";
 import "../Styles/App.css";
 import "../Styles/CardStyle.css";
-import logo from "../Assets/econotravel-logo.png";
 import modernista from "../Assets/Modernista.png";
-function Card(){
+import App from '../App.js';
+
+function Card({experience}){
+
+
+
     return(
             <div className="card-container">
                 <figure>
                     <img className="image" src={modernista} alt="photo"/>
                     <div className="hover-layer">
                     <button className="btn btn-icon">
-                        <i className="far fa-eye"></i>
+                        <i className="far fa-eye"/>
                         <span>view more</span>
                     </button>
                         <button className="btn btn-icon">
-                            <i className="far fa-edit"></i>
+                            <i className="far fa-edit"/>
                             <span>edit</span>
                         </button>
                         <button className="btn btn-icon">
-                            <i className="far fa-trash-alt"></i>
+                            <i  className="far fa-trash-alt"/>
                             <span>delete</span>
                         </button>
                     </div>
                 </figure>
                 <div className="card-description">
-                    <h2 className="card-title">Descubre la Barcelona Modernista de Noche</h2>
-                    <p className="card-price">280€</p>
+                   <h2 className="card-title"> {experience.name}</h2>
+                    <p className="card-price">{experience.price + '€'}</p>
                     <p className="card-price-detail">por persona</p>
+                    <p>{experience.duration}</p>
                 </div>
                 <div className="card-buttons">
                     <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">Book Now</button>
