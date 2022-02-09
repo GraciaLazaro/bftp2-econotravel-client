50c09fc4f7be21d29ec18a5431dcb327f7f1113d
Candy <candymdeoleo@gmail.com>
2022-02-09 12:41:52 +0100

    React router issues solved. Footer and header showing in all routes!

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, {useEffect, useState} from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 import AboutUs from "./components/AboutUs";
 import Enquires from "./components/Enquires";
 import Form from "./components/Form";
@@ -49,9 +49,10 @@ function App() {
             <Header onButtonClicked={() => setShowForm(true)} />
             { showForm && <Form onSubmit={e => addExperience(e)} onClose={()=>setShowForm(false)}/>}
             <Routes>
-                <Route index element={<Home experiences={experiences} deleteExperience={deleteExperience} />}/>
+                <Route path="/" index element={<Home experiences={experiences} deleteExperience={deleteExperience} />}/>
                 <Route path="/AboutUs" element={<AboutUs />} />
                 <Route path="/Enquires" element={<Enquires />} />
+                <Route path="*" element={<Navigate replace to="/"/>} />
             </Routes>
             <Footer />
         </BrowserRouter>
